781f9692c948ecfdb7d9edbec0e39d852a89b1dc
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2022-03-01 19:39:35 +0100

    Initial SpringInitializer Commit

---
+++ src/main/java/com/mvcplan/mvcplantool/MvcPlanToolApplication.java
@@ -0,0 +1,13 @@
+package com.mvcplan.mvcplantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPlanToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcPlanToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvcplan/mvcplantool/MvcPlanToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvcplan.mvcplantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcPlanToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
