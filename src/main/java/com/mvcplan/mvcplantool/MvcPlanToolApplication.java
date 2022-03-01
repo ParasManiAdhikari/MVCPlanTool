cfbaed07629c099a9f7be6013cfdf94bc43f28eb
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2022-03-01 20:23:55 +0100

    html testing

--- src/main/java/com/mvcplan/mvcplantool/MvcPlanToolApplication.java
+++ src/main/java/com/mvcplan/mvcplantool/MvcPlanToolApplication.java
@@ -2,9 +2,16 @@ package com.mvcplan.mvcplantool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@RestController
 public class MvcPlanToolApplication {
+//    @GetMapping("/")
+//    String home() {
+//        return "Spring is here!";
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(MvcPlanToolApplication.class, args);
